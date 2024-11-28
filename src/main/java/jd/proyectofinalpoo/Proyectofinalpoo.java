ea6587359f3c4e05f60058e2b7baeb526492937f
User <User@Laptop-JD>
2024-11-27 22:11:56 -0500

    Inicial

---
+++ src/main/java/jd/proyectofinalpoo/Proyectofinalpoo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package jd.proyectofinalpoo;
+
+/**
+ *
+ * @author User
+ */
+public class Proyectofinalpoo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
