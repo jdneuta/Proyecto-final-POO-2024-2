14bf1f5e3c8f0f1ff6076ddf023b8f648b11d2b3
stivencastillo12 <161136895+stivencastillo12@users.noreply.github.com>
2024-11-28 20:30:24 -0500

    Prueba IV en esta prueba se crean las clases de reserva y servicio junto con sus resoectivas vistas respetando el patron mvc

--- src/main/java/jd/proyectofinalpoo/Proyectofinalpoo.java
+++ src/main/java/jd/proyectofinalpoo/Proyectofinalpoo.java
@@ -3,6 +3,9 @@
  */
 
 package jd.proyectofinalpoo;
+import Modelo.*;
+import Vista.*;
+
 
 /**
  *
@@ -13,5 +16,22 @@ public class Proyectofinalpoo {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         // prueba 2
+        //Prueba 3
+        System.out.println("Olacarebola");
+        //Prueba numero 4
+        //Stiven:
+        //1.creo la clase Usuario en el package de modelo
+        //2.creo la vista del usuario 
+        //creacion del usuario 
+        Usuario  us1 = new Usuario("Alan", "alan@unicauca.edu", "3132789213", "1", "03121");
+        //creacion de la vista del usuario
+        vista_usuario vus1 = new vista_usuario();
+        //mostramos los detalles del usuario creando la vista
+        vus1.mostrar_datos_usuario(us1);
+        //Creacion de un servicio adicional
+        Servicio serv1 = new Servicio("Spa", "Relájate en nuestro exclusivo spa, donde encontrarás tratamientos revitalizantes, masajes relajantes y un ambiente diseñado para tu bienestar total.", 10000, true);
+        //Creacion de la vista del servicio
+        Vista_servicio vser1= new Vista_servicio();
+        vser1.vista_servicios(serv1);
     }
 }
