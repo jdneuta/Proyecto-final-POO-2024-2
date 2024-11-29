294d84f6fc5b779d006b1329d0ad737d9ae549fb
stivencastillo12 <161136895+stivencastillo12@users.noreply.github.com>
2024-11-28 23:13:11 -0500

    Prueba V se hicieron todas las clases implementando sus atributos en vista estan los metodos  de mostrar la informacion NO SE IMPLEMENTARON LOS METODOS DE TODAS LAS CLASES

--- src/main/java/jd/proyectofinalpoo/Proyectofinalpoo.java
+++ src/main/java/jd/proyectofinalpoo/Proyectofinalpoo.java
@@ -7,6 +7,7 @@ import Modelo.*;
 import Vista.*;
 
 
+
 /**
  *
  * @author User
@@ -14,12 +15,6 @@ import Vista.*;
 public class Proyectofinalpoo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        // prueba 2
-        //Prueba 3
-        System.out.println("Olacarebola");
-        //Prueba numero 4
-        //Stiven:
         //1.creo la clase Usuario en el package de modelo
         //2.creo la vista del usuario 
         //creacion del usuario 
@@ -29,9 +24,15 @@ public class Proyectofinalpoo {
         //mostramos los detalles del usuario creando la vista
         vus1.mostrar_datos_usuario(us1);
         //Creacion de un servicio adicional
-        Servicio serv1 = new Servicio("Spa", "Relájate en nuestro exclusivo spa, donde encontrarás tratamientos revitalizantes, masajes relajantes y un ambiente diseñado para tu bienestar total.", 10000, true);
+        Servicio serv1 = new Servicio("Spa", "Relajate en nuestro exclusivo spa, donde encontraras tratamientos revitalizantes, masajes relajantes y un ambiente disenado para tu bienestar total.", 10000, true);
         //Creacion de la vista del servicio
         Vista_servicio vser1= new Vista_servicio();
         vser1.vista_servicios(serv1);
+        //PRUEBA 5
+        // creamos una habitacion
+        Habitacion habit1 = new Habitacion(0, "Suit de rey", 100000, 5, true, "Suite de lujo con diseno exclusivo, vistas panoramicas, jacuzzi privado y amenidades premium para una experiencia inolvidable.");
+        //Creacion de la vista 
+        Vista_Habitacion Vhabit1 = new Vista_Habitacion();
+        Vhabit1.vista_habitacion(habit1);
     }
 }
